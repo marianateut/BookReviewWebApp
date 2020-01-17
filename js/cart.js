a7159a091307637540a78ee938d20a3ab7e621d4
Mariana Teut <mary_marianailea@yahoo.com>
2020-01-17 16:02:32 +0200

    initial commit

---
+++ js/cart.js
@@ -0,0 +1,55 @@
+window.Cart = {
+    API_BASE_URL: "http://localhost:8085",
+
+    getCart:function () {
+        var userId = 39;
+        $.ajax({
+            url: Cart.API_BASE_URL + "/carts/" + userId
+
+          //  method: "GET"
+
+        }).done(function (response) {
+            console.log(response);
+            Cart.displayBooks(response.books);
+        })
+    },
+
+
+    displayBooks:function (books) {
+        var allBooksHtml = "";
+        books.forEach(book => allBooksHtml += Cart.getBookHtml(book));
+        $(".shop_table.cart tbody").html(allBooksHtml);
+    },
+
+    getBookHtml:function (book) {
+        return `<tr class="cart_item">
+                         <td class="product-remove">
+                             <a title="Remove this item" class="remove" href="#">×</a> 
+                         </td>
+                         <td class="product-thumbnail">
+                             <a href="single-product.html"><img width="145" height="145" alt="poster_1_up" class="shop_thumbnail" src="image/book2.png"></a>
+                         </td>
+                         <td class="product-name">
+                             <a href="single-product.html">${book.title}</a> 
+                         </td>
+                         <td class="product-price">
+                             <span class="amount">${book.price}</span> 
+                         </td>
+                         <td class="product-quantity">
+                             <div class="quantity buttons_added">
+                                 <input type="button" class="minus" value="-">
+                                 <input type="number" size="4" class="input-text qty text" title="Qty" value="1" min="0" step="1">
+                                 <input type="button" class="plus" value="+">
+                             </div>
+                         </td>
+                         <td class="product-subtotal">
+                             <span class="amount">${book.price}</span> 
+                         </td>
+                    </tr>`
+    },
+
+
+};
+
+
+Cart.getCart();
---
+++ js/shop.js
@@ -0,0 +1,68 @@
+window.Shop = {
+    API_BASE_URL: "http://localhost:8085",
+
+    getBooks:function () {
+        $.ajax({
+           // method: "GET" ,
+            url: Shop.API_BASE_URL + "/books"
+
+        }).done (function (response) {
+            console.log(response)
+            Shop.displayBooks(response.content);
+        });
+    },
+
+
+    displayBooks:function (books) {
+        var allBooksHtml = "";
+        books.forEach(book => allBooksHtml += Shop.getBooksHtml(book));
+        $(".single-product-area .row:first-child") .html(allBooksHtml);
+    },
+
+    getBooksHtml:function (book) {
+        return `<div class="col-md-3 col-sm-6">
+                    <div class="single-shop-product">
+                        <div class="product-upper">
+                            <img src="image/book1.png" alt="">
+                        </div>
+                        <h2><a href="">${book.title}</a></h2>
+                        <div class="product-carousel-price">
+                            <ins>$${book.price}</ins>
+                        </div>
+
+                        <div class="product-option-shop">
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${book.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                        </div>
+                    </div>
+                </div>`
+    },
+
+    addBookToCart: function (bookId) {
+        //userId to be read from memory somehow in the future
+        var requestBody = {
+            UserId: 39,
+            bookId: bookId
+        };
+
+        $.ajax({
+            url: Shop.API_BASE_URL + "/carts",
+            method:"PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            window.location.replace("cart.html")
+        }) //asa fac navigatie spre alta pagina
+    },
+
+        bindEvents: function () {
+            $(".single-product-area").delegate(".add_to_cart_button", "click", function (event) {
+                event.preventDefault();
+
+                let bookId = $(this).data("book_id");
+                Shop.addBookToCart(bookId);
+            })
+        }
+};
+
+Shop.getBooks();
+Shop.bindEvents();
